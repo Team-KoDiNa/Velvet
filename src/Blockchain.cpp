812aa3fca91a0c98564aaab5801f0cfc4fed2338
KiRist <37296174+KiRist-code@users.noreply.github.com>
2021-02-26 21:45:55 +0900

    Add files via upload

---
+++ src/Blockchain.cpp
@@ -0,0 +1,16 @@
+#include "Blockchain.h"
+
+Blockchain::Blockchain() {
+    _vChain.emplace_back(Block(0,"Genesis Block"));
+    _nDifficulty = 6;
+}
+
+void Blockchain::AddBlock(Block bNew){
+    bNew.sPrevHash = _GetLastBlock().GetHash();
+    bNew.MineBlock(_nDifficulty);
+    _vChain.push_back(bNew);
+}
+
+Block Blockchain::_GetLastBlock() const {
+    return _vChain.back();
+}
