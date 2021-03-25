bdd598d154552b99e25991261dcc61dbfbc203aa
KiRist <37296174+KiRist-code@users.noreply.github.com>
2021-03-25 23:38:26 +0900

    Update Buffer.h

--- src/Buffer.h
+++ src/Buffer.h
@@ -35,4 +35,53 @@ public:
         }
         buffer = new dtype[length];
     }
+    
+    int size() const{
+        return length;
+    }
+    
+    dtype* getBuffer() const{
+        return buffer;
+    }
+    
+    BaseBuffer(){
+        length = 0;
+        buffer = nullptr;
+    }
+    
+    BaseBuffer(int size) : buffer(nullptr){
+        reserve(size);
+    }
+    
+    BaseBuffer(BaseBuffer&& o) noexcept{
+        length = o.length;
+        buffer = o.buffer;
+        
+        o.length = 0;
+        o.buffer = nullptr;
+    }
+    
+    BaseBuffer& operator=(BaseBuffer&& o) noexcept {
+        length = o.length;
+        buffer = o.buffer;
+        
+        o.length = 0;
+        o.buffer = nullptr;
+        
+        return *this;
+    }
+    
+    ~BaseBuffer(){
+        if(buffer) delete[] buffer;
+    }
+};
+
+using CBuffer = BaseBuffer<char>;
+
+template <class T, int N>
+class <#class name#> {
+    <#instance variables#>
+    
+public:
+    <#member functions#>
 };
