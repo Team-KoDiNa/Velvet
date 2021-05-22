c70e87fe48f16d1e6e060cd58d379022fee83fd4
KiRist <37296174+KiRist-code@users.noreply.github.com>
2021-05-23 00:40:17 +0900

    Update blockchain.go

--- block/blockchain.go
+++ block/blockchain.go
@@ -3,3 +3,26 @@ package velvet
 type BlockChain struct {
 	blocks []*Block
 }
+
+var halving = 1
+
+func (bc *BlockChain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.prevHash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *BlockChain {
+	return &BlockChain{[]*Block{NewGenesisBlock()}}
+}
+
+func (bc *BlockChain) isHalving() int {
+	if len(bc.blocks)%230000 == 0 {
+		halving = halving * 2
+	}
+	return halving
+}
